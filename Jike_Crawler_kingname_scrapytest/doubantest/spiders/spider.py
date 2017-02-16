058e2f8dfa0fd4224c71c3240aba649c5e889993
tianhui <zhufuzz@hotmail.com>
2017-02-16 15:46:59 -0500

    pp

---
+++ Jike_Crawler_kingname_scrapytest/doubantest/spiders/spider.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# from scrapy.contrib.spiders import CrawlSpider
+from scrapy.spiders import CrawlSpider
+
+
+class Douban(CrawlSpider):
+    name = "doubanTest"
+    start_urls = ['http://movie.douban.com/top250'
+    ]
+    def parse(self,response):
+        print response.body
+        print response.url
+        #print "ddd"
+        #a = response.url
+        #b = 1
+
+#
